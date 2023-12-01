c9cd7cd3804fd4e8a9b69face456a2764f68ee04
Nikhil Prakash <115891140+nikhilljpskj@users.noreply.github.com>
2023-12-01 11:34:39 +0530

    Update Decision.py

--- Decision.py
+++ Decision.py
@@ -1,33 +1,22 @@
 from sklearn.datasets import load_iris
-
 from sklearn import metrics
-
-from sklearn import tree
-
+from sklearn.model_selection import train_test_split
+from sklearn.tree import DecisionTreeClassifier, plot_tree
 import matplotlib.pyplot as plt
 
-from sklearn.model_selection import train_test_split
+iris = load_iris()
+x = iris.data
+y = iris.target
 
-from sklearn tree import DecisionTreeClassifier
+x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.3, random_state=1)
 
-iris=load_iris()
+clf = DecisionTreeClassifier()
+clf.fit(x_train, y_train)
 
-x=iris.data
+y_pred = clf.predict(x_test)
 
-y=iris.target
+print("Accuracy: ", metrics.accuracy_score(y_test, y_pred))
 
-x_trainx_test,y_train,y_test = train_test_split(x,y,test_size=0.3,random_state=1)
-
-clf-DecisionTreeClassifier()
-
-clf-clf fit(x_train_y_train)
-
-y_pred-clf.predict(x_test)
-
-print("Accuracy: ",metrics.accuracy_score(y_test,y_pred))
-
-plt.figure(figsize=(15,15))
-
-tree.plot_tree(clf fontsize=10,filled=True,rounded=True,class_names-iris.target_names, fa ture_names=iris.feature_names)
-
+plt.figure(figsize=(15, 15))
+plot_tree(clf, fontsize=10, filled=True, rounded=True, class_names=list(iris.target_names), feature_names=iris.feature_names)
 plt.show()
